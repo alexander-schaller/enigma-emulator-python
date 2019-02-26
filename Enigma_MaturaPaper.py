920ac8747f5ba1200db26028ff1c3ad14cb44760
alexanderschaller <alexjschaller@gmail.com>
2019-02-26 21:31:56 +0100

    Added some useful additions

--- Enigma_MaturaPaper.py
+++ Enigma_MaturaPaper.py
@@ -1,32 +1,29 @@
 import json
 from copy import *
-
 ##ScramblerWiring info is opened --> and json information extracted
 ScramblerText = open('ScramblerText.txt', 'r')
 Scramblers = json.load(ScramblerText)
 
-
 ##Main Function
-def Enigma(SL, SP, PB, RP, word):
+def Enigma (SL, SP, PB, RP, word):
     """SL (Scrambler Location, List of 3 Scramblers)
     SP (Scrambler Position, List of 3 Letters you can see on the machine)
     PB (Plugboard, Dictionary with 0 - 13 Letters connecting with eachother)
     RP (Ring Position, List of 3 Numbers from 1 - 26)
     l (Input letter)"""
-    SL1, SL2, SL3 = ScramblerNum[SL[0]], ScramblerNum[SL[1]], ScramblerNum[
-        SL[2]]  ## Turns the Three roman numerals of the scrambler into Numbers
-    SP1, SP2, SP3 = ord(SP[0]) - 65, ord(SP[1]) - 65, ord(SP[2]) - 65
+    SL1, SL2, SL3 = ScramblerNum[SL[0]], ScramblerNum[SL[1]], ScramblerNum[SL[2]] ## Turns the Three roman numerals of the scrambler into Numbers
+    SP1, SP2, SP3 = ord(SP[0]) - 65, ord(SP[1]) - 65, ord(SP[2]) - 65 ##Turn State into Numbers
     ## Creates the 3 Scramblers with the specific information
-    S1 = Scrambler(3, Scramblers[SL1], Scramblers[6][SL1], SP1, RP[0])  ##Left scrambler in the machine
-    S2 = Scrambler(2, Scramblers[SL2], Scramblers[6][SL2], SP2, RP[1])  ##Middle Scrambler in the machine
-    S3 = Scrambler(1, Scramblers[SL3], Scramblers[6][SL3], SP3, RP[2])  ##Right Scrambler in the machine
-    BP = {value: key for key, value in PB.items()}  # Creating a Second Dictionary with the reversed keys
-    ScramblerText.close()  # Important to close the File --> Otherwise it corupts
-    S1.RingSetting, S2.RingSetting, S3.RingSetting
+    S1 = Scrambler(3, Scramblers[SL1], [SL1, SL2, SL3], SP1, RP[0]) ##Left scrambler in the machine
+    S2 = Scrambler(2, Scramblers[SL2], [SL1, SL2, SL3], SP2, RP[1]) ##Middle Scrambler in the machine
+    S3 = Scrambler(1, Scramblers[SL3], [SL1, SL2, SL3], SP3, RP[2]) ##Right Scrambler in the machine
+    BP = {value: key for key, value in PB.items()} #Creating a Second Dictionary with the reversed keys
+    ScramblerText.close() #Important to close the File -->  Otherwise it corupts
+    S1.RingSetting, S2.RingSetting, S3.RingSetting ##Applies ring setting
 
-    for x in range(100):
+    for x in range(100): ##Only for debugging
         l = "a"
-        l = l.upper()  ##First I change all the letters that are inputted to an uppercase letter for simplicity and originality
+        l = l.upper() ##First I change all the letters that are inputted to an uppercase letter for simplicity and originality
         pbo = Plugboard(l, PB, BP)
         so = S1.forward(S2.forward(S3.forward(pbo)))
         rf0 = Scramblers[5][so]
@@ -43,19 +40,20 @@ def Enigma(SL, SP, PB, RP, word):
 
 
 class Scrambler(object):
-    def __init__(self, Position, Wiring, TurnOverNotch, State, RingPosition):
-        # Declaring the Variables for the class
+    def __init__(self, Position, Wiring, Scramb, State, RingPosition):
+        #Declaring the Variables for the class
         self.Position = Position
         self.Wiring = Wiring
-        self.TurnOverNotch = TurnOverNotch
+        self.Scramb = Scramb
+        self.TurnOverNotch = Scramblers[6]
         self.State = State
         self.RingPosition = RingPosition
-        self.counter = 0
 
     def RingSetting(self):
+        """Changes the ring setting"""
         c = copy(self.Wiring)
         ring = self.RingPosition - 1
-        if ring == 0:
+        if ring == 0 :
             return self.wiring
         else:
             for i in range(ring):
@@ -64,40 +62,36 @@ class Scrambler(object):
             return self.Wiring
 
     def forward(self, letter):
-        self.counter += 1
+        """Foreward movement through the """
         lnumber = ord(letter) - 65
+        self.State = self.State % 26
         if self.Position == 1:
             self.State += 1
-            letter_new = chr(((ord(self.Wiring[chr((lnumber + self.State) % 26 + 65)]) - 65 - self.State) % 26) + 65)
+            letter_new = chr(((ord(self.Wiring[chr((lnumber + self.State)%26 + 65)]) - 65 - self.State )% 26) + 65)
         elif self.Position == 2:
-            if self.counter == 26:
+            if self.State == ord(self.TurnOverNotch[self.Scramb[0]])-65:
                 self.State += 1
-                letter_new = chr(
-                    ((ord(self.Wiring[chr((lnumber + self.State) % 26 + 65)]) - 65 - self.State) % 26) + 65)
+                letter_new = chr(((ord(self.Wiring[chr((lnumber + self.State)%26 + 65)]) - 65 - self.State )% 26) + 65)
             else:
-                letter_new = chr(
-                    ((ord(self.Wiring[chr((lnumber + self.State) % 26 + 65)]) - 65 - self.State) % 26) + 65)
+                letter_new = chr(((ord(self.Wiring[chr((lnumber + self.State)%26 + 65)]) - 65 - self.State )% 26) + 65)
         elif self.Position == 3:
-            if self.counter == (ord(self.TurnOverNotch) - 65 + 26):
+            if self.State == ord(self.TurnOverNotch[self.Scramb[1]])-65:
                 self.State += 1
-                letter_new = chr(
-                    ((ord(self.Wiring[chr((lnumber + self.State) % 26 + 65)]) - 65 - self.State) % 26) + 65)
+                letter_new = chr(((ord(self.Wiring[chr((lnumber + self.State)%26 + 65)]) - 65 - self.State )% 26) + 65)
             else:
-                letter_new = chr(
-                    ((ord(self.Wiring[chr((lnumber + self.State) % 26 + 65)]) - 65 - self.State) % 26) + 65)
+                letter_new = chr(((ord(self.Wiring[chr((lnumber + self.State)%26 + 65)]) - 65 - self.State )% 26) + 65)
         return letter_new
 
     def backward(self, letter):
         lnumber = ord(letter) - 65
         reverse = {value: key for key, value in self.Wiring.items()}
         if self.Position == 1:
-            letter_new = chr(((ord(reverse[chr((lnumber + self.State) % 26 + 65)]) - 65 - self.State) % 26) + 65)
+            letter_new = chr(((ord(reverse[chr((lnumber + self.State)%26 + 65)]) - 65 - self.State) % 26) + 65)
         elif self.Position == 2:
-            letter_new = chr(((ord(reverse[chr((lnumber + self.State) % 26 + 65)]) - 65 - self.State) % 26) + 65)
+            letter_new = chr(((ord(reverse[chr((lnumber + self.State)%26 + 65)]) - 65 - self.State) % 26) + 65)
         elif self.Position == 3:
-            letter_new = chr(((ord(reverse[chr((lnumber + self.State) % 26 + 65)]) - 65 - self.State) % 26) + 65)
+            letter_new = chr(((ord(reverse[chr((lnumber + self.State)%26 + 65)]) - 65 - self.State) % 26) + 65)
         return letter_new
-
 
 def Plugboard(l, PB, BP):
     ##If the letter is in the Plugboard(PB) then it changes its value otherwise it keeps it.
@@ -110,20 +104,21 @@ def Plugboard(l, PB, BP):
     return pbo
 
 
+
 ##Changes roman numerals to Python Indices
-ScramblerNum = {"I": 0, "II": 1, "III": 2, "IV": 3, "V": 4}
+ScramblerNum = {"I":0,"II":1,"III":2,"IV":3,"V":4}
 
 ##Input Values
-SL = ["I", "II", "III"]
-SP = ["A", "B", "C"]
+SL = ["I","II","III"]
+SP = ["A","B","C"]
 PB = {
-    "A": "G",
-    "H": "Q",
-    "O": "C",
-    "Z": "Y",
-    "N": "M",
-    "T": "K"
+    "A":"G",
+    "H":"Q",
+    "O":"C",
+    "Z":"Y",
+    "N":"M",
+    "T":"K"
 }
-RP = [1, 1, 1]
+RP = [1,1,1]
 ##Call Function Enigma
 Enigma(SL, SP, PB, RP, "A")
