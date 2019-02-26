a7ede727d34a57ad3269e1f846c15a9d8459cb1c
alexanderschaller <alexjschaller@gmail.com>
2019-02-26 21:23:43 +0100

    Continued working on rotator function

--- Enigma_MaturaPaper.py
+++ Enigma_MaturaPaper.py
@@ -4,9 +4,10 @@ ScramblerText = open('ScramblerText.txt', 'r')
 Scramblers = json.load(ScramblerText)
 
 ##Main Function
-def Enigma(SL, SP, PB, l):
+def Enigma(SL, SP, IR, PB, l):
     """SL will be a list of 3 numbers ranging from 1-3 which is the Location of the scramblers
     SP will be a list of three numbers ranging from 1-26 (For letters A-Z) which is the letter on each of the Scramblers
+    IR is the setting of the Index Ring
     PB will be the Plugboard setting, being a dictionary of 6 letters with their corresponding letter
     l is a letter that has to be encoded with enigma"""
     l = l.upper()
@@ -15,6 +16,7 @@ def Enigma(SL, SP, PB, l):
     SL1, SL2, SL3 = SL[0], SL[1], SL[2]
     ##Plugboard function, Scrambler Function used 3 times in a row with different scramblers, Reflector then checked wether output rf is in the reverse plugboard or not
     S1, S2, S3 = ScramblerNum[SL[0]], ScramblerNum[SL[1]], ScramblerNum[SL[2]]
+    Rotator(SL, SP)
     pbo = Plugboard(l)
     so = Scrambler(Scrambler(Scrambler(pbo, S3), S2), S1)
     rf = Reflector(so,[S1, S2, S3])
@@ -42,21 +44,35 @@ def Plugboard(l):
         pbo = l
     return pbo
 
-def Rotator(f, cp):
+def Rotator(SL, cp):
     """F is the Scrambler Layout, while cp is is the current position of the scrambler"""
     ##cp is there to be used with the notch so it can turn at an apropriate position
-    for i in range(26):
-        i = i + 65
-    Scramblers[f[0]][chr(i)] = Scramblers[f[0]][chr(i + 1)]
-    return Scramblers[f[0]]
-    if cp[0] = #dont know yet :
+    ##ton is equal to the turnover notch
+    ton = ["Q","E","V","J","Z"]
+    S1, S2, S3 = ScramblerNum[SL[0]], ScramblerNum[SL[1]], ScramblerNum[SL[2]]
+    ##logic behind this is that if it reaches the turnover notch it makes the wheel turn, therefore making the next wheel move once
+    s1 = Scramblers[S1]["A"]
+    s2 = Scramblers[S2]["A"]
+    s3 = Scramblers[S3]["A"]
+    ##work with modulo in numbers 26
+    for i in range(25):
+        if i < 25:
+            Scramblers[S1][chr(i + 65)] = Scramblers[S1][chr(i + 66)]
+        else:
+            Scramblers[S1][chr(i + 65)] = s1
+    return Scramblers[S1]
+    if cp[0] == ton[f[0]] :
         for i in range(26):
-            i = i + 65
-        Scramblers[f[1]][chr(i)] = Scramblers[f[1]][chr(i + 1)]
-    if cp[1] = #dont know yet :
+            if i < 25:
+                Scramblers[S2][chr(i + 65)] = Scramblers[S2][chr(i + 66)]
+            else:
+                Scramblers[S2][chr(i + 65)] = s2
+    if cp[1] == ton[f[1]] :
         for i in range(26):
-            i = i + 65
-        Scramblers[f[2]][chr(i)] = Scramblers[f[2]][chr(i + 1)]
+            if i < 25:
+                Scramblers[S3][chr(i + 65)] = Scramblers[S3][chr(i + 6)]
+            else:
+                Scramblers[S3][chr(i + 65)] = s3
 
 
 def Reflector(l, f):
@@ -82,7 +98,7 @@ PB = {"A":"G",
       "Z":"Y",
       "N":"M",
       "T":"K"}
+IR = 0
 
 ##Call Function Enigma
-print("Output Letter: " + Enigma(SL, SP, PB, "h"))
-print(Rotator())
+print("Output Letter: " + Enigma(SL, SP, IR, PB, "t"))
