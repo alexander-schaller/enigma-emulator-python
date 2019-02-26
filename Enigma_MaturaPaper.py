a6d3af8b930b465337846cbac5e9e5f1d54c63c1
alexanderschaller <alexjschaller@gmail.com>
2019-02-26 21:17:44 +0100

    Added some new functions (Still not working)

--- Enigma_MaturaPaper.py
+++ Enigma_MaturaPaper.py
@@ -3,38 +3,58 @@ def Enigma(SL, SP, PB, l):
 SP will be a list of three numbers ranging from 1-26 (For letters A-Z) which is the letter on each of the Scramblers
 PB will be the Plugboard setting, being a dictionary of 6 letters with their corresponding letter
 l is a letter that has to be encoded with enigma"""
+    l = l.upper()
     ##3 Scramblers in a row that have the 3 different positions specified in list SL
     so = Scrambler(Scrambler(Scrambler(l, SL[0]), SL[1]), SL[2])
-    return so
+    pbo = Plugboard(so)
+    return pbo
+
 
 ##I will try and complete the scrambler functions using just dictionaries at this point in time it looks like it's the most plausible choice
-def Scrambler(l,n):
+def Scrambler(l, n):
     """Takes letter l and scrambler # (n) as input"""
     ##Here I take n which is the number of the scrambler and l which is the letter that is inputted and search for its counter part in the Scramblers list
     return Scramblers[n - 1][l]
-        
+
+
+def Plugboard(l):
+    """Takes letter l and checks for it in the plugboard"""
+    if l in PB:
+        pbo = PB[l]
+    else:
+        pbo = l
+    return pbo
+
+
 ##Test Values
 
-Scramblers = [{"A":"A","B":"B","C":"C","D":"D","E":"E","F":"F","G":"G","H":"H","I":"I","J":"J","K":"K","L":"L",
-    "M":"M","N":"N","O":"O","P":"P","Q":"Q","R":"R","S":"S","T":"T","U":"U","V":"V","W":"W","X":"X",
-    "Y":"Y","Z":"Z"},
-              {"A":"A","B":"B","C":"C","D":"D","E":"E","F":"F","G":"G","H":"H","I":"I","J":"J","K":"K","L":"L",
-    "M":"M","N":"N","O":"O","P":"P","Q":"Q","R":"R","S":"S","T":"T","U":"U","V":"V","W":"W","X":"X",
-    "Y":"Y","Z":"Z"},
-              {"A":"A","B":"B","C":"C","D":"D","E":"E","F":"F","G":"G","H":"H","I":"I","J":"J","K":"K","L":"L",
-    "M":"M","N":"N","O":"O","P":"P","Q":"Q","R":"R","S":"S","T":"T","U":"U","V":"V","W":"W","X":"X",
-    "Y":"Y","Z":"Z"}]
+Scramblers = [
+    {"A": "D", "B": "M", "C": "T", "D": "W", "E": "S", "F": "I", "G": "L", "H": "R", "I": "U", "J": "Y", "K": "Q",
+     "L": "N",
+     "M": "K", "N": "F", "O": "E", "P": "J", "Q": "C", "R": "A", "S": "Z", "T": "B", "U": "P", "V": "G", "W": "X",
+     "X": "O",
+     "Y": "H", "Z": "V"},
+    {"A": "H", "B": "Q", "C": "Z", "D": "G", "E": "P", "F": "J", "G": "T", "H": "M", "I": "O", "J": "B", "K": "L",
+     "L": "N",
+     "M": "C", "N": "I", "O": "F", "P": "D", "Q": "Y", "R": "A", "S": "W", "T": "V", "U": "E", "V": "U", "W": "S",
+     "X": "R",
+     "Y": "K", "Z": "X"},
+    {"A": "U", "B": "Q", "C": "N", "D": "T", "E": "L", "F": "S", "G": "Z", "H": "F", "I": "M", "J": "R", "K": "E",
+     "L": "H",
+     "M": "D", "N": "P", "O": "X", "P": "K", "Q": "I", "R": "B", "S": "V", "T": "Y", "U": "G", "V": "J", "W": "C",
+     "X": "W",
+     "Y": "O", "Z": "A"}]
 
-SL = [1,2,3]
-SP = [26,26,26]
-PB = {"A":"A",
-      "B":"B",
-      "C":"C",
-      "D":"D",
-      "E":"E",
-      "F":"F"}
+SL = [1, 2, 3]
+SP = [26, 26, 26]
+PB = {"A": "G",
+      "H": "Q",
+      "O": "C",
+      "Z": "Y",
+      "N": "M",
+      "T": "K"}
 
 ##Test Area
 
 ##print(Scrambler("B", 1))
-print(Enigma(SL, SP, PB, "H"))
+print(Enigma(SL, SP, PB, "h"))
